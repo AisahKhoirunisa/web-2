eae21237fdb4897b07300ecd77ec938de841f956
AisahKhoirunisa <aisahkh01@gmail.com>
2025-02-20 12:39:04 +0700

    praktikum01

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Pemrograman Web 2
+
 ## Repositori Pratikum Pemrograman Web 2
 
-- NAMA : AISAH KHOIRUNISA     
+- NAMA : AISAH KHOIRUNISA
 - NIM : 0110124066
-- ROMBEL : SI11  
+- ROMBEL : SI11
---
+++ variable_user.php
@@ -0,0 +1,12 @@
+<?php
+// mendefinisikan variable
+$nama = 'Aisah';
+$umur = 19;
+$berat = 50;
+
+echo'Nama saya ' . $nama;
+echo'<br/>Umur : ' . $umur . '  Tahun';
+echo'<br/>Berat : ' . $berat;
+
+?>
+
