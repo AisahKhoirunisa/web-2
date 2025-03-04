6dcde27610b00e310c964a9efa98d63ead29629c
AisahKhoirunisa <aisahkh01@gmail.com>
2025-03-04 13:14:25 +0700

    Tugas praktikum 2

--- praktikum-2b/form_belanja.php
+++ praktikum-2b/form_belanja.php
@@ -6,14 +6,13 @@
     <title>Form Belanja</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <form class="form-horizontal" method="POST" action="total_belanja.php">
 </head>
 <body>
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-8">
                 <h2>Belanja Online</h2>
-                <form method="POST" action="form_belanja.php">
+                <form method="POST" action="total_belanja.php">  <!-- Form hanya ada di sini -->
                     <div class="form-group row">
                         <label for="customer" class="col-4 col-form-label">Nama Customer</label> 
                         <div class="col-8">
@@ -23,7 +22,7 @@
                                         <i class="fa fa-address-book"></i>
                                     </div>
                                 </div> 
-                                <input id="customer" name="customer" placeholder="Nama Lengkap" type="text" class="form-control" required="required">
+                                <input id="customer" name="customer" placeholder="Nama Lengkap" type="text" class="form-control" required>
                             </div>
                         </div>
                     </div>
@@ -31,15 +30,15 @@
                         <label class="col-4">Pilih Produk</label> 
                         <div class="col-8">
                             <div class="custom-control custom-radio custom-control-inline">
-                                <input name="produk" id="produk_0" type="radio" class="custom-control-input" value="TV" required="required"> 
+                                <input name="produk" id="produk_0" type="radio" class="custom-control-input" value="TV" required> 
                                 <label for="produk_0" class="custom-control-label">TV</label>
                             </div>
                             <div class="custom-control custom-radio custom-control-inline">
-                                <input name="produk" id="produk_1" type="radio" class="custom-control-input" value="KULKAS" required="required"> 
+                                <input name="produk" id="produk_1" type="radio" class="custom-control-input" value="KULKAS" required> 
                                 <label for="produk_1" class="custom-control-label">KULKAS</label>
                             </div>
                             <div class="custom-control custom-radio custom-control-inline">
-                                <input name="produk" id="produk_2" type="radio" class="custom-control-input" value="MESIN CUCI" required="required"> 
+                                <input name="produk" id="produk_2" type="radio" class="custom-control-input" value="MESIN CUCI" required> 
                                 <label for="produk_2" class="custom-control-label">MESIN CUCI</label>
                             </div>
                         </div>
@@ -53,7 +52,7 @@
                                         <i class="fa fa-shopping-cart"></i>
                                     </div>
                                 </div> 
-                                <input id="jumlah" name="jumlah" placeholder="Jumlah" type="number" class="form-control" required="required">
+                                <input id="jumlah" name="jumlah" placeholder="Jumlah" type="number" class="form-control" required>
                             </div>
                         </div>
                     </div>
